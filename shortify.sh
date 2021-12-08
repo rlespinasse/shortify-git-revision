f4ee0c06304a60fecf85a66edb84df89418bb729
Romain Lespinasse <romain.lespinasse@gmail.com>
2021-12-08 21:19:06 +0100

    feat: support prepend text to variable names

--- shortify.sh
+++ shortify.sh
@@ -4,8 +4,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   # On MacOS,
   # bash don't support substitution, so we use 'tr'
   NAME=$(echo "$INPUT_NAME" | tr '[:lower:]' '[:upper:]')
+  PREFIX=$(echo "$INPUT_PREFIX" | tr '[:lower:]' '[:upper:]')
 else
   NAME=${INPUT_NAME^^}
+  PREFIX=${INPUT_PREFIX^^}
 fi
 REVISION=${INPUT_REVISION:-${!NAME}}
 
@@ -15,8 +17,8 @@ fi
 
 if [ "$(git cat-file -e "$REVISION" 2>&1)" == "" ]; then
   {
-    echo "${NAME}=${REVISION}"
-    echo "${NAME}_SHORT=$(git rev-parse --short "$REVISION")"
+    echo "${PREFIX}${NAME}=${REVISION}"
+    echo "${PREFIX}${NAME}_SHORT=$(git rev-parse --short "$REVISION")"
   } >>"$GITHUB_ENV"
 elif [ "${INPUT_CONTINUE_ON_ERROR}" == "false" ]; then
   echo "::error ::Invalid revision: $REVISION from $NAME"
