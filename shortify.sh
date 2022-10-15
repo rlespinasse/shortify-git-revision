3b1b863e54d4ecfdb4b86f8b76e9635a1e3fd595
Romain Lespinasse <romain.lespinasse@gmail.com>
2022-10-15 21:26:27 +0200

    fix: use environment file to manage outputs

--- shortify.sh
+++ shortify.sh
@@ -45,8 +45,8 @@ elif [ "${INPUT_CONTINUE_ON_ERROR}" == "false" ]; then
 fi
 
 if [ "${SHORT_PUBLICATION}" == "true" ]; then
-  echo "::set-output name=revision::${REVISION}"
-  echo "::set-output name=short::${SHORT_VALUE}"
+  echo "revision=${REVISION}" >> "$GITHUB_OUTPUT"
+  echo "short=${SHORT_VALUE}" >> "$GITHUB_OUTPUT"
 
   if [ "${INPUT_PUBLISH_ENV}" == "true" ]; then
     {
